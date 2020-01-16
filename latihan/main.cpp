c4d1c181c5a53105981f64667438f5da7b0d6faa
fahrul31 <59928245+fahrul31@users.noreply.github.com>
2020-01-16 12:57:14 +0800

    bikin project

---
+++ latihan/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+float f;
+long l;
+double d;
+long double ld;
+
+bool bl;
+char c;
+string s;
+
+int arr[10];
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    int i;
+
+    cout<<"angka i = "<<i<<"\n";
+    return 0;
+}
